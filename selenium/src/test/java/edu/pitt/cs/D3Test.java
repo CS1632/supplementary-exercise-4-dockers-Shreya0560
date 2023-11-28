b8802b290ab2f6b821f120a4921dbbbc83b04cb1
Shreya18334 <SHA94@pitt.edu>
2023-11-28 13:28:17 -0500

    Update D3Test.java

--- selenium/src/test/java/edu/pitt/cs/D3Test.java
+++ selenium/src/test/java/edu/pitt/cs/D3Test.java
@@ -36,10 +36,10 @@ public class D3Test {
     ChromeOptions options = new ChromeOptions();
     options.addArguments("--headless");
     driver = new ChromeDriver(options); 
-    
-    driver = new ChromeDriver();
+
+    /* driver = new ChromeDriver();
     js = (JavascriptExecutor) driver;
-    vars = new HashMap<String, Object>();
+    vars = new HashMap<String, Object>(); */
   }
   @After
   public void tearDown() {
